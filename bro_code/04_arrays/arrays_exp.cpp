3f53bd4579a0efcb5fa22b0b9f7cce810f85ab48
Adegbite Adeoye <adegbite.adeoye0@gmail.com>
2023-09-19 19:03:31 +0100

    Add test for sizeof

--- bro_code/04_arrays/arrays_exp.cpp
+++ bro_code/04_arrays/arrays_exp.cpp
@@ -11,8 +11,16 @@ int main()
     prices[1] = 67000.50;
     prices[2] = 24000.00;
 
-    for (size_t i = 0; i < sizeof(prices) / sizeof(prices[0]); i++)
-    {
-        std::cout << "Car - " << cars[i] << ", Price = " << prices[i] << "\n";
-    }
+    // for (size_t i = 0; i < sizeof(prices) / sizeof(prices[0]); i++)
+    // {
+    // std::cout << "Car - " << cars[i] << ", Price = " << prices[i] << "\n";
+    // }
+
+    std::string name = "Mr. Man";
+    double gpa = 2.5;
+    int scores[] = {34, 5556, 4333, 5444, 0, 7644};
+
+    std::cout << "Scores size " << sizeof(scores) << " bytes \n";
+    std::cout << sizeof(int) << " bytes \n";
+    std::cout << "There are " << sizeof(scores) / sizeof(scores[0]) << " elements \n";
 }
